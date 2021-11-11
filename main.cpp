2beb71ea638dfe0fb03a3570d6542476bc24c3e3
ColinWeatherly <colrweat@ut.utm.edu>
2021-11-11 13:23:55 -0600

    Adding a modified main.cpp with new code

--- main.cpp
+++ main.cpp
@@ -8,7 +8,32 @@
 
 using namespace std;
 
+int sum(int n);
+/* Description:
+ *    This function sums all integers from 1 to n using
+ *    a loop and returns this sum.
+ *
+ * Parameters:
+ *    int n
+ *       The top limit of the sum.
+ *
+ * Postconditions:
+ *    All integers from 1 to n are summed and returned.
+ */
+
 int main() {
-  cout << "Hello World!" << endl;
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  cout << sum(n) << endl;
   return 0;
 }
+
+int sum(int n) {
+  int totalSum = 0;
+  for (int i = 1; i <= n; i++) {
+    totalSum += i;
+  }
+  return totalSum;
+}
+    
